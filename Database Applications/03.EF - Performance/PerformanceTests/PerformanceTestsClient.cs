b1ff8df179cc2651d394dee2dfab073ab95ce791
itCherokee <simeonov23@yahoo.com>
2015-03-15 18:07:35 +0200

    DB Apps - Third homework

---
+++ Database Applications/03.EF - Performance/PerformanceTests/PerformanceTestsClient.cs
@@ -0,0 +1,164 @@
+﻿namespace PerformanceTests
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class PerformanceTestsClient
+    {
+        public static void Main()
+        {
+            //// -------------------------------------------------------
+            //// Problem 1: Show Data from Related Tables
+            //// -------------------------------------------------------
+            //// slow version
+            TaskOneSlow();
+
+            //// optimized version
+            TaskOneOptimized();
+
+            //// -------------------------------------------------------
+            //// Problem 2. Play with ToList()
+            //// -------------------------------------------------------
+            //// slow version
+            TaskTwoSlow();
+
+            //// optimized version
+            TaskTwoOptimized();
+
+            //// -------------------------------------------------------
+            //// Problem 3. Select Everything vs. Select Certain Columns
+            //// -------------------------------------------------------
+            //// slow version
+            TaskThreeSelectAll();
+
+            //// optimized version
+            TaskThreeSelectOnlyTitle();
+        }
+
+        private static void TaskOneSlow()
+        {
+            using (var context = new AdsEntities())
+            {
+                var ads = context.Ads.ToList();
+
+                foreach (var ad in ads)
+                {
+                    Console.WriteLine(
+                        "{0}:\n\t- Status: {1};\n\t- Category: {2};\n\t- Town: {3};\n\t- User: {4}",
+                        ad.Title,
+                        ad.AdStatus.Status,
+                        ad.Category == null ? "(none)" : ad.Category.Name,
+                        ad.Town == null ? "(none)" : ad.Town.Name,
+                        ad.AspNetUser.Name);
+                }
+            }
+        }
+
+        private static void TaskOneOptimized()
+        {
+            using (var context = new AdsEntities())
+            {
+                var ads = context.Ads.Include(c => c.Category).Include(t => t.Town).Include(u => u.AspNetUser).ToList();
+
+                foreach (var ad in ads)
+                {
+                    Console.WriteLine(
+                        "{0}:\n\t- Status: {1};\n\t- Category: {2};\n\t- Town: {3};\n\t- User: {4}",
+                        ad.Title,
+                        ad.AdStatus.Status,
+                        ad.Category == null ? "(none)" : ad.Category.Name,
+                        ad.Town == null ? "(none)" : ad.Town.Name,
+                        ad.AspNetUser.Name);
+                }
+            }
+        }
+
+        private static void TaskTwoSlow()
+        {
+            using (var context = new AdsEntities())
+            {
+                var ads = context.Ads
+                    .ToList()
+                    .Where(s => s.AdStatus.Status == "Published")
+                    .Select(n => new
+                    {
+                        Title = n.Title,
+                        Category = n.Category == null ? "(none)" : n.Category.Name,
+                        Town = n.Town == null ? "(none)" : n.Town.Name,
+                        PublishDate = n.Date
+                    })
+                    .ToList()
+                    .OrderBy(o => o.PublishDate);
+
+                foreach (var ad in ads)
+                {
+                    Console.WriteLine(
+                        "{0} (Published on: {1}):\n\t- Category: {2};\n\t- Town: {3};",
+                        ad.Title,
+                        ad.PublishDate,
+                        ad.Category,
+                        ad.Town);
+                }
+            }
+        }
+
+        private static void TaskTwoOptimized()
+        {
+            using (var context = new AdsEntities())
+            {
+                var ads = context.Ads
+                    .Where(s => s.AdStatus.Status == "Published")
+                    .Select(n => new
+                    {
+                        Title = n.Title,
+                        Category = n.Category == null ? "(none)" : n.Category.Name,
+                        Town = n.Town == null ? "(none)" : n.Town.Name,
+                        PublishDate = n.Date
+                    })
+                    .OrderBy(o => o.PublishDate)
+                    .ToList();
+
+                foreach (var ad in ads)
+                {
+                    Console.WriteLine(
+                        "{0} (Published on: {1}):\n\t- Category: {2};\n\t- Town: {3};",
+                        ad.Title,
+                        ad.PublishDate,
+                        ad.Category,
+                        ad.Town);
+                }
+            }
+        }
+
+        private static void TaskThreeSelectAll()
+        {
+            using (var context = new AdsEntities())
+            {
+                var timeElapsed = DateTime.Now;
+                var ads = context.Ads.ToList();
+
+                // foreach (var ad in ads)
+                // {
+                //     Console.WriteLine("Ad title: {0}", ad.Title);
+                // }
+                Console.WriteLine("Time elapsed (selecting all): {0}", DateTime.Now - timeElapsed);
+            }
+        }
+
+        private static void TaskThreeSelectOnlyTitle()
+        {
+            using (var context = new AdsEntities())
+            {
+                var timeElapsed = DateTime.Now;
+                var ads = context.Ads.Select(t => t.Title).ToList();
+
+                // foreach (var title in ads)
+                // {
+                //     Console.WriteLine("Ad title: {0}", title);
+                // }
+                Console.WriteLine("Time elapsed (selecting just Title): {0}", DateTime.Now - timeElapsed);
+            }
+        }
+    }
+}
---
+++ Database Applications/04.EF - Transactions/NewsDatabase.Client/NewsClient.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using NewsDatabase.Model;
+using NewsDtabase.Data;
+
+namespace NewsDatabase.Client
+{
+    public class NewsClient
+    {
+        public static void Main()
+        {
+            using (var context = new NewsDbContext())
+            {
+                // Problem 2:
+                // Step 1.	At startup, the app should load from the DB the news text 
+                //          and print it on the console.
+                Console.WriteLine("Application started.");
+                foreach (var item in context.News)
+                {
+                    Console.WriteLine("Text from DB: - {0}", item.Content);
+                }
+
+                bool isConcurrentConflict = true;
+                while (isConcurrentConflict)
+                {
+                    // Step 2.	After that, the app should enter a new value for the news text 
+                    //          from the console.
+                    var lastNews = context.News.OrderByDescending(o => o.NewsId).FirstOrDefault();
+                    Console.Write("Enter the corrected text (last news): ");
+                    lastNews.Content = Console.ReadLine() ?? "New news";
+
+                    // Step 3.	After entering a new value, the app should try to save it to the DB.
+                    //          -   In case of success (no conflicting updates), the app should say that 
+                    //              the changes were saved and should finish its work.
+                    //          -   In case of concurrent update conflict, the app should display 
+                    //              an error message, should display the new (changed) text from 
+                    //              the DB and should go to Step 2.
+                    try
+                    {
+                        context.SaveChanges();
+                        Console.WriteLine("Changes successfully saved in the DB.");
+                        isConcurrentConflict = false;
+                    }
+                    catch (DbUpdateConcurrencyException ex)
+                    {
+                        Console.Write("Conflict! Text from DB (last news): {0}.", lastNews.Content);
+                        Console.ReadKey();
+                    } 
+                }
+            }
+        }
+    }
+}
