8bb1581b23f10ac61dee56ee557652e3f1a19f32
itCherokee <simeonov23@yahoo.com>
2016-02-23 16:11:09 +0200

    no idea

---
+++ Open Courses/Python/01.Intro/01.05.StopIterations.py
@@ -0,0 +1,8 @@
+import turtle
+g = 134
+l = 120
+t = 100
+while t > 0:
+    turtle.left(g)
+    turtle.forward(l)
+    t = t - 1
---
+++ Open Courses/Python/01.Intro/01.07.ChessBoard.py
@@ -0,0 +1,9 @@
+import turtle
+g = 134
+l = 120
+t = 100
+while t > 0:
+    turtle.left(g)
+    turtle.forward(l)
+    t = t - 1
+
